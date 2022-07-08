d9bcfc2c0c63364209060ab489767b0df4625528
Artem Sabetski <artsabets@mail.ru>
2022-07-08 17:21:52 +0300

    edit sql-init.xml

--- migration/src/main/resources/db/changeLog/1.0.0/sql/document.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-CREATE TABLE main.document (
-    id integer PRIMARY KEY,
-    number varchar(40) UNIQUE,
-    user_id integer NULL,
-    FOREIGN KEY (user_id) REFERENCES main.user (id)
-)
--- migration/src/main/resources/db/changeLog/1.0.0/sql/role.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
-CREATE TABLE main.role (
-    id integer PRIMARY KEY,
-    code varchar(20) UNIQUE
-);
